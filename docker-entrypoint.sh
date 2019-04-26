958a2a3fa1e80fe7966c65e620d7560583a0d368
Roman Iuvshyn <riuvshyn@redhat.com>
2019-04-26 12:43:06 +0300

    fix overwriting nginx.conf with envsubst
    fix overwriting nginx.conf with envsubst

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,7 @@
 
 [ -z "$LISTEN_PORT" ] && export LISTEN_PORT=8080
 
-envsubst '$${FORWARD_HOST}' < nginx.conf > /tmp/nginx.conf
-envsubst '$${LISTEN_PORT}' < nginx.conf > /tmp/nginx.conf
+envsubst '$${FORWARD_HOST} $${LISTEN_PORT}' < nginx.conf > /tmp/nginx.conf
 envsubst < auth.htpasswd > /tmp/auth.htpasswd
 
 nginx -c /tmp/nginx.conf
