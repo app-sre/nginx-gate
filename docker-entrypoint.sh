07705f80023b54de8603b57bcfd72d986610e898
Bhushan Thakur <bhthakur@redhat.com>
2024-06-28 11:37:26 -0400

    add proxy buffer configuration (#12)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,12 +10,16 @@
 
 [ -z "$CACHE_KEY_INACTIVE_TIME" ] && export CACHE_KEY_INACTIVE_TIME=60m
 
-[ -z "$PROXY_BUFFERING" ] && export PROXY_BUFFERING=off
+[ -z "$PROXY_BUFFERS_NUMBER" ] && export PROXY_BUFFERS_NUMBER=8
+
+[ -z "$PROXY_BUFFERS_SIZE" ] && export PROXY_BUFFERS_SIZE=8k
+
+[ -z "$PROXY_BUFFER_SIZE" ] && export PROXY_BUFFER_SIZE=16k
 
 [ ! -z "${BASIC_AUTH_USERNAME}" ] && [ ! -z "${BASIC_AUTH_PASSWORD}" ] && \
     export HTPASSWD=$(htpasswd -bn "${BASIC_AUTH_USERNAME}" "${BASIC_AUTH_PASSWORD}")
 
-envsubst '$${FORWARD_HOST} $${LISTEN_PORT} $${METRICS_PATH} $${CACHE_MAX_SIZE} $${CACHE_TTL} $${CACHE_KEYS_ZONE_SIZE} $${CACHE_KEY_INACTIVE_TIME} $${PROXY_BUFFERING} ' < nginx.conf > /tmp/nginx.conf
+envsubst '$${FORWARD_HOST} $${LISTEN_PORT} $${METRICS_PATH} $${CACHE_MAX_SIZE} $${CACHE_TTL} $${CACHE_KEYS_ZONE_SIZE} $${CACHE_KEY_INACTIVE_TIME} $${PROXY_BUFFERS_NUMBER} $${PROXY_BUFFERS_SIZE} $${PROXY_BUFFER_SIZE}' < nginx.conf > /tmp/nginx.conf
 envsubst < auth.htpasswd > /tmp/auth.htpasswd
 
 if [ "${BASIC_AUTH_DISABLE}" = "true" ]
